e934aa5255ce3ebe3c3cd97fe76f26e8dad6d86c
Fernando Oliveira <fernando@fholiveira.com>
2014-07-12 12:20:04 -0300

    Creating UI for 'System Start and Shutdown' settings

--- tlp/views/categories/system_start_and_shutdown.py
+++ tlp/views/categories/system_start_and_shutdown.py
@@ -5,3 +5,21 @@ class SystemStartAndShutdown():
         loader.connect(self)
         self.menu = loader.get('system_start_and_shutdown_row')
         self.panel = loader.get('system_start_and_shutdown_panel')
+        
+        self.startup_actions = loader.get('actions_on_startup')
+        
+        self.startup = {'disabled' : loader.get('disable_devices_on_startup'), 
+                        'enabled' : loader.get('enable_devices_on_startup')}
+
+        self.shutdown = {'disabled' : loader.get('disable_devices_on_shutdown'), 
+                         'enabled' : loader.get('enable_devices_on_shutdown')}
+
+    def change_restore_devices_on_startup(self, switch, gparam):
+        self.startup_actions.set_sensitive(not switch.get_active())
+
+    def _get_devices(self, container):
+        devices = [device.get_name() for device in container.get_children()
+                   if not device.get_active()]
+
+        return ' '.join(devices)
+
